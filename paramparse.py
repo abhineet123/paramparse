1f55fbabc564e498f3c15cabe16d57c98fec61fe
abhineet123 <asingh1@ualberta.ca>
2019-12-14 13:19:28 -0700

    Several fixes in the section names printing

--- paramparse.py
+++ paramparse.py
@@ -519,13 +519,16 @@ def process(obj, args_in=None, cmd=True, cfg='', cfg_root='', cfg_ext='',
                         _end_id = section_ids[_sec_id + 1] if _sec_id < len(sections) - 1 else n_file_args
                         _sec_args += file_args[_start_id:_end_id]
 
-                        _cfg_sec_disp.append((x, _start_id + 1, _end_id))
+                        _cfg_sec_disp.append('{}: {} -> {}'.format(x, _start_id + 1, _end_id))
 
                     invalid_cfg_sec = [k for k in _cfg_sec if k not in valid_cfg_sec]
                     if invalid_cfg_sec:
                         raise AssertionError('Invalid cfg sections provided:\n {}'.format(invalid_cfg_sec))
 
-                    print('Reading from section(s):\n{}'.format(_cfg_sec_disp))
+                    print('\t{}'.format(
+                        '\n\t'.join(_cfg_sec_disp)
+                        # pformat(_cfg_sec_disp)
+                    ))
 
                     file_args = _sec_args
 
