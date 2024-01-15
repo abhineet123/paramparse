8b21a6dc5092c88917427b9289337c7ceff1b15a
abhineet123 <asingh1@ualberta.ca>
2024-01-15 12:25:10 -0700

    updating to version 1.8.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("ReadMe.md", "r") as fh:
 
 setuptools.setup(
     name="paramparse",
-    version="1.7.9",
+    version="1.8.0",
     author="Abhineet Singh",
     author_email="abhineet.iiita@gmail.com",
     description="argparse wrapper to allow hierarchically nested class based parameters",
